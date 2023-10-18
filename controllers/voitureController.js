1751d2588043acba5dae4fe340a006814f4cb736
jonathan <jonathanguerpillon@gmail.com>
2023-10-18 23:05:01 +0200

    add function getvoiture

--- controllers/voitureController.js
+++ controllers/voitureController.js
@@ -7,6 +7,16 @@ const getVoitures = async (req,res) => {
     res.status(200).send(parent);
 };
 
+const getVoiture = async (req,res) => {
+    const voitureId = req.params.id;
+    const voitureDatas = await sequelize.query("SELECT * FROM voitures WHERE id =:voitureId", {
+        replacements: { voitureId },
+        type: sequelize.QueryTypes.SELECT,
+    }); 
+    res.status(200).send(voitureDatas);
+};
+
 module.exports = {
-    getVoitures
+    getVoitures,
+    getVoiture
 }
--- routes/VoitureRoute.js
+++ routes/VoitureRoute.js
@@ -2,5 +2,6 @@ const voitureController = require('../controllers/voitureController.js');
 const router = require('express').Router();
 
 router.get("/getvoitures", voitureController.getVoitures);
+router.get("/getvoiture/:id", voitureController.getVoiture);
 
 module.exports = router;
