b6a763c1fc6a1327a59ad9a41adb6c8c625590f9
jonathan <jonathanguerpillon@gmail.com>
2023-10-19 11:14:49 +0200

    fix bug procedure stockée in function getSportsOfUser

--- controllers/sportController.js
+++ controllers/sportController.js
@@ -4,7 +4,9 @@ const Sports = db.sport;
 
 const getSportsOfUser = async (req, res) => {
     const userId = req.params.id;
-    const SportsUser = await sequelize.query('CALL ObtenirLesSportsDeLutilisateur (:userId)', {remplacements: { userId: userId }})
+    console.log(userId)
+    // const SportsUser = await sequelize.query(`CALL ObtenirLesSportsDeLutilisateur(${userId})`, {remplacements: [userId], type: sequelize.QueryTypes.RAW,})
+    const SportsUser = await sequelize.query('CALL ObtenirLesSportsDeLutilisateur(:userId)', {replacements: {userId}, type: sequelize.QueryTypes.RAW,})
     res.status(200).send(SportsUser);
 };
 
