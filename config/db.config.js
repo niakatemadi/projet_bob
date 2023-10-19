b3f2801da59193b51a6f9ba34b139527ff38d4b0
Madi niakate <madi.niakate@accenture.com>
2023-10-19 11:29:56 +0200

    new configggg

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,7 @@
 module.exports = {
     HOST : 'localhost',
-<<<<<<< HEAD
     USER : 'root',
     PASSWORD : '',
-=======
-    USER : 'admin',
-    PASSWORD : 'admin',
->>>>>>> 13382456333a94a885d36ce0cb385a574cba1d50
     DB : 'bobeponge2',
-    dialect : 'mysql',
-    PORT: '8889'
+    dialect : 'mysql'
 };
